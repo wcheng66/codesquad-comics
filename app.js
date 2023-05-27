2c4bfbf4c69224d9da239a930ff31879c6e06ffd
chengwei0815 <chengwei0815@gmail.com>
2023-05-27 10:12:49 -0400

    update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.json());
 app.use(methodOverride('_method'))
 app.use(morgan('combined'));
 // app.use(routes);
